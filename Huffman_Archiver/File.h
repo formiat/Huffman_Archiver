9a08018c1eb000fdf16e608537f21cf6af7a3a81
Alexander <formiat95@gmail.com>
2018-03-27 17:50:50 +0300

    All MyLib::File methods definitions was moved from source file to header file.

--- Huffman_Archiver/File.h
+++ Huffman_Archiver/File.h
@@ -2,37 +2,41 @@
 
 #include "TypeAliases.h"
 #include <cstdio>
+#include <utility>
 
 
 namespace MyLib
 {
 
+using namespace std;
+
+
 class File
 {
 public:
-	File(const char* fileName, const char* mode);
+	File(const char* fileName, const char* mode) { fopen_s(&handle, fileName, mode); }
 	File(const File& file) = delete;
-	File(File&& file);
+	File(File&& file) { this->handle = file.handle; file.handle = nullptr; }
 
-	~File();
+	~File() { close(); }
 
 	std::FILE* getHandle() { return handle; }
 
-	byte read();
-	void write(byte b);
+	byte read() { byte b; fread(&b, sizeof b, 1, handle); return b; }
+	void write(byte b) { fwrite(&b, sizeof b, 1, handle); }
 	
-	long tell() const;
+	long tell() const { return ftell(handle); }
 	// Return true if success
-	bool seek(long offset, int origin);
-	void rewind();
+	bool seek(long offset, int origin) { return !fseek(handle, offset, origin); }
+	void rewind() { std::rewind(handle); }
 	// Return true if EOF
-	bool eof() const;
+	bool eof() const { return feof(handle); }
 
 	// Return true if success
-	bool close();
+	bool close() { return !fclose(handle); }
 
 	File& operator=(const File& file) = delete;
-	File& operator=(File&& file);
+	File& operator=(File&& file) { std::swap(this->handle, file.handle); return *this; }
 
 private:
 	std::FILE* handle;
